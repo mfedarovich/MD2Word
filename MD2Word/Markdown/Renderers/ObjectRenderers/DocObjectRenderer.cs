97b4aef6b3e96a3c21af8214b191b39e6d5b3234
Mikhail <mifenator@gmail.com>
2021-10-25 15:51:03 +0300

    clean-up: DocObjectRenderer.cs

--- MD2Word/Markdown/Renderers/ObjectRenderers/DocObjectRenderer.cs
+++ MD2Word/Markdown/Renderers/ObjectRenderers/DocObjectRenderer.cs
@@ -3,13 +3,27 @@ using Markdig.Syntax;
 
 namespace MD2Word.Markdown.Renderers.ObjectRenderers
 {
-    public abstract class DocObjectRenderer<TObject> : MarkdownObjectRenderer<DocRenderer, TObject> where TObject : MarkdownObject
+    public abstract class DocObjectRenderer<TObject> : IMarkdownObjectRenderer 
+        where TObject : MarkdownObject
     {
         protected IDocument Document { get; }
 
-        public DocObjectRenderer(IDocument document)
+        protected DocObjectRenderer(IDocument document)
         {
             Document = document;
         }
+
+        public bool Accept(RendererBase renderer, MarkdownObject obj)
+        {
+            return obj is TObject;
+        }
+
+        public void Write(RendererBase renderer, MarkdownObject objectToRender)
+        {
+            Write((DocRenderer)renderer, (TObject)objectToRender);
+        }
+        
+        protected abstract void Write(DocRenderer renderer, TObject obj);
+
     }
 }
