c14cc0b3e21e215183090852a82fe41f0e73a141
Mikhail <mifenator@gmail.com>
2021-10-29 13:08:44 +0300

    ImageInlineTests.cs

---
+++ MD2Word.Tests/ImageInlineTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace MD2Word
+{
+    [TestFixture]
+    public class ImageInlineTests : BaseTest
+    {
+        [TestCase("![](a)")]
+        [TestCase(" ![](a)")]
+        [TestCase("![](a) ")]
+        [TestCase(" ![](a) ")]
+        [TestCase("   ![description](http://example.com)")]
+        public void Test(string value)
+        {
+            TestOutput(value, value);
+        }
+
+        [TestCase("paragraph   ![description](http://example.com)")]
+        public void TestParagraph(string value)
+        {
+            TestOutput(value, value);
+            
+        }
+    }
+}
